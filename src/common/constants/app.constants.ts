0159cf3ddb3b3c8032361854a44e048c5b2aac1a
lee22417 <jlee22417@gmail.com>
2025-10-08 05:48:51 +0900

    🔧 constants 파일 업데이트

--- src/common/constants/app.constants.ts
+++ src/common/constants/app.constants.ts
@@ -1,3 +1,4 @@
 // 앱 전역 상수
 
 export const DEFAULT_PAGE_SIZE = 20;
+export const SLOW_QUERY_THRESHOLD = 500; // slow query 임계값 500ms (임계값은 필요에 따라 조정)
--- src/common/constants/enum.constants.ts
+++ src/common/constants/enum.constants.ts
@@ -1,11 +1,27 @@
+// space.price_type
 export enum SPACE_PRICE_TYPE {
   FIXED = 'FIXED',
   PER_PERSION = 'PER_PERSION',
 }
 
+// reservation.status
 export enum RESERVATION_STATUS {
   OCCUPIED = 'OCCUPIED', // 임시 점유 (예약 중, 예약완료X, 결제X)
   PENDING = 'PENDING', // 결제 대기 (예약완료, 결제 대기 상태 (현금))
   COMPLETED = 'COMPLETED', // 결제 완료
   CANCELED = 'CANCELED', // 결제 취소
+}
+
+// pay.pay_status
+export enum PayStatus {
+  PENDING = 'PENDING', // 결제 대기
+  COMPLETED = 'COMPLETED', // 결제 완료
+  CANCELED = 'CANCELED', // 취소
+}
+
+// pay.pay_method
+export enum PayMethod {
+  CARD = 'CARD',
+  CASH = 'CASH',
+  FREE = 'FREE',
 }
